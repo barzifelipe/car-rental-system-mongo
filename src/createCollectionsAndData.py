adea189feab0efe3f60b7f2f756a76f1b66d5173
emanoel vitor <emanoelvitor020@gmail.com>
2025-11-26 19:32:08 -0300

    createCollections e relatorios - Emanoel

---
+++ src/createCollectionsAndData.py
@@ -0,0 +1,54 @@
+import logging as l
+from conexion.mongo_queries import MongoQueries
+from conexion.oracle_queries import OracleQueries
+import json
+
+LISTA_COLECOES = ["carros", "cliente", "funcionario", "locacoes"]
+logger = l.getLogger(name="Example_CRUD_MongoDB")
+logger.setLevel(level=l.WARNING)
+mongo = MongoQueries()
+
+
+def createCollections(drop_if_exists:bool=False):
+
+    mongo.connect()
+    existing_collections = mongo.db.list_collection_names()
+    for colecao in LISTA_COLECOES:
+        if colecao in existing_collections:
+            if drop_if_exists:
+                mongo.db.drop_collection(colecao)
+                logger.warning(f"{colecao} droppada!")
+                mongo.db.create_collection(colecao)
+                logger.warning(f"{colecao} criada!")
+        else:
+            mongo.db.create_collection(colecao)
+            logger.warning(f"{colecao} criada!")
+    
+    mongo.close()
+
+def insert_many(data:json, colecao:str):
+    mongo.connect()
+    mongo.db[colecao].insert_many(data)
+    mongo.close()
+
+
+def extract_and_insert():
+    oracle = OracleQueries()
+    oracle.connect()
+    sql = "select * from labdatabase.{table}"
+    for colecao in LISTA_COLECOES:
+        df = oracle.sqlToDataFrame(sql.format(table=colecao))
+        if colecao == "locacoes":
+            df["DATA_INICIO"] = df["DATA_INICIO"].dt.strftime("%m-%d-%Y")
+            df["DATA_FIM"] = df["DATA_FIM"].dt.strftime("%m-%d-%Y")
+        logger.warning(f"Dados extraídos do banco de dados oracle labdatabase.{colecao}")
+        records = json.loads(df.T.to_json()).values()
+        logger.warning("Dados convertidos para json")
+        insert_many(data=records, colecao=colecao)
+        logger.warning(f"documentos gerados em {colecao}")
+
+if __name__ == "__main__":
+    l.warning("Iniciando")
+    createCollections(drop_if_exists=True)
+    extract_and_insert()
+    l.warning("Finalizado!")
--- src/reports/relatorios.py
+++ src/reports/relatorios.py
@@ -1,4 +1,7 @@
 from conexion.oracle_queries import OracleQueries
+from conexion.mongo_queries import MongoQueries
+import pandas as pd
+from pymongo import ASCENDING, DESCENDING
 
 class Relatorio:
     def __init__(self):
@@ -17,6 +20,9 @@ class Relatorio:
         with open("/home/labdatabase/Documents/car_rental_system/src/sql/relatorio_total_valor_diarias.sql") as f:
             self.query_relatorio_total_valor_diarias = f.read()
 
+    
+   # INICIO DOS RELATÓRIOS DO ORACLE !!!!!!!!
+    
     def get_relatorio_carros_sistema(self):
         oracle = OracleQueries()
         oracle.connect()
@@ -47,3 +53,122 @@ class Relatorio:
         print(oracle.sqlToDataFrame(self.query_relatorio_total_valor_diarias))
         input("\nPressione Enter para prosseguir ou sair\n")
 
+
+# FIM DOS RELATÓRIOS ORACLE !!!!
+
+#INICO DOS RELATÓRIOS DO MONGO !!
+
+# CLIENTES MONGO
+def get_relatorio_clientes_mongo(self):
+    mongo = MongoQueries()
+    mongo.connect()
+    query_result = mongo.db["clientes"].find({}, 
+                                             {"ID_CLIENTE": 1,
+                                              "NOME_CLIENTE": 1,
+                                              "CPF": 1,
+                                              "_id":0
+                                            }).sort("NOME_CLIENTE", ASCENDING)
+
+    df_cliente = pd.DataFrame(list(query_result))
+    mongo.close()
+    print(df_cliente)
+    input("Pressione Enter para sair do relatório de clientes")
+
+
+
+#FUNCIONARIOS MONGO
+def get_relatorio_funcionarios_mongo(self):
+    mongo = MongoQueries()
+    mongo.connect()
+    query_result = mongo.db["funcionarios"].find({}, 
+                                                {"ID_FUNCIONARIO": 1,
+                                                 "NOME": 1,
+                                                 "CARGO": 1,
+                                                 "_id": 0
+                                                }).sort("nome", ASCENDING)
+    
+    df_funcionario = pd.DataFrame(list(query_result))
+    mongo.close()
+    print(df_funcionario)
+    input("Pressioone Enter para sair do relatório de funcionários")
+
+
+#CARROS MONGO
+def get_relatorios_carros_mongo(self):
+    mongo = MongoQueries()
+    mongo.connect()
+    query_results = mongo.db["carros"].find({}, 
+                                            {"ID_CARRO": 1,
+                                             "MODELO": 1,
+                                             "PLACA": 1,
+                                             "CATEGORIA": 1,
+                                             "VALOR_DIARIA": 1,
+                                             "_id": 0
+                                            }).sort("MODELO", ASCENDING)
+    
+    df_carro = pd.DataFrame(list(query_results))
+    mongo.close()
+    print(df_carro)
+    input("Pressione Enter para sair do relatório de carros")
+
+
+#LOCACOES MONGO
+def get_relatorio_locacoes_mongo(self):
+    mongo = MongoQueries()
+    mongo.connect()
+    query_result = mongo.db["locacoes"].aggregate([
+                                                  {
+                                                      '$lookup':{
+                                                          'from': 'clientes',
+                                                          'localField': 'ID_CLIENTE',
+                                                          'foreignField': 'ID_CLIENTE',
+                                                          'as': 'cliente'
+                                                      }
+                                                  }, {
+                                                      '$unwind':{
+                                                          'path': '$cliente'
+                                                      }
+                                                  },{
+                                                      '$lookup': {
+                                                          'from': 'carros',
+                                                          'localField': 'id_carro',
+                                                          'foreignField': 'id_carro',
+                                                          'as': 'carro'
+                                                      }
+                                                  }, {
+                                                      '$unwind':{
+                                                          'path': '$carro'
+                                                      }
+                                                  }, {
+                                                      '$lookup':{
+                                                          'from': 'funcionarios',
+                                                          'localField': 'id_funcionario',
+                                                          'foreignField': 'id_funcionario',
+                                                          'as': 'funcionario'
+                                                      }
+                                                  }, {
+                                                      '$unwind':{
+                                                          'path': '$funcionario'
+                                                      }
+                                                  }, {
+                                                      '$project':{
+                                                          '_id': 0,
+                                                          'numero_reserva': 1,
+                                                          'data_inicio': {
+                                                              '$dateToString': {'format: "%d/%m/%Y", date: $data_inicio'}
+                                                          },
+                                                          'data_fim': {
+                                                              '$dateToString': {'format: "%d/%m/%Y", date: $data_fim'}
+                                                          },
+                                                          'nome_cliente': '$cliente.nome_cliente',
+                                                          'modelo_carro': '$carro.modelo',
+                                                          'nome_funcionario': '$funcionario.nome'
+                                                      }
+                                                  },
+                                                  {'$sort': {'numero_reserva': 1}}
+                                                 ])
+    
+    df_locaco = pd.DataFrame(list(query_result))
+    mongo.close()
+    print(df_locaco[["numero_reserva", "data_inicio", "data_fim", "nome_cliente", "modelo_carro", "nome_funcionario"]])
+    input("Pressione Enter para sair do relatório de locações")
